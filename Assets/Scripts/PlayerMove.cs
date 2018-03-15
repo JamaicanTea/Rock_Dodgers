04a596e6e2049210a842d482b39cbefb5d053bc6
Acxell Leandro <aleandro803@gmail.com>
2018-03-15 15:59:22 +1100

    more anims

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,8 @@ public class PlayerMove : MonoBehaviour {
 	public float moveSpeed = 1000f;
 	Rigidbody rb;
 	Animator anim;
+	public bool moveForward = false;
+	public bool moveSides = false;
 
 	// Use this for initialization
 	void Start () 
@@ -20,20 +22,44 @@ public class PlayerMove : MonoBehaviour {
 		if (Input.GetKey (KeyCode.W)) 
 		{
 			rb.velocity = Vector3.forward * moveSpeed * Time.deltaTime * 60;
+			moveForward = true;
 		}
+		else if (Input.GetKeyUp (KeyCode.W)) 
+		{
+			moveForward = false;
+		}
+
 		if (Input.GetKey (KeyCode.A)) 
 		{
 			rb.velocity = Vector3.left * moveSpeed * Time.deltaTime * 60;
+			moveSides = true;
 		}
+		else if (Input.GetKeyUp (KeyCode.A)) 
+		{
+			moveSides = false;
+		}
+
 		if (Input.GetKey (KeyCode.S)) 
 		{
 			rb.velocity = Vector3.back * moveSpeed * Time.deltaTime * 60;
+			moveForward = true;
+		} 
+		else if (Input.GetKeyUp (KeyCode.S)) 
+		{
+			moveForward = false;
 		}
+
 		if (Input.GetKey (KeyCode.D)) 
 		{
 			rb.velocity = Vector3.right * moveSpeed * Time.deltaTime * 60;
+			moveSides = true;
 		}
-		if (Input.GetKeyDown (KeyCode.Space)) 
+		else if (Input.GetKeyUp (KeyCode.D)) 
+		{
+			moveSides = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && moveForward == true) 
 		{
 			anim.SetTrigger("RollNow");
 		}
