6c2c64d18c50cfa425aa3879940d83663642256d
Acxell Leandro <aleandro803@gmail.com>
2018-03-15 14:46:53 +1100

    Movement Script added

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public float moveSpeed = 1000f;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			rb.velocity = Vector3.forward * moveSpeed * Time.deltaTime * 60;
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			rb.velocity = Vector3.left * moveSpeed * Time.deltaTime * 60;
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			rb.velocity = Vector3.back * moveSpeed * Time.deltaTime * 60;
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			rb.velocity = Vector3.right * moveSpeed * Time.deltaTime * 60;
+		}
+
+	}
+}
